fc55d71f9729234966e60c8848967204b714a06f
John Luo <jt.luo@mail.utoronto.ca>
2016-10-05 17:49:45 -0700

    Feedback

--- src/Microsoft.AspNetCore.ResponseCaching.Abstractions/IResponseCacheFeature.cs
+++ src/Microsoft.AspNetCore.ResponseCaching.Abstractions/IResponseCacheFeature.cs
@@ -5,8 +5,14 @@ using Microsoft.Extensions.Primitives;
 
 namespace Microsoft.AspNetCore.ResponseCaching
 {
+    /// <summary>
+    /// A feature for configuring additional response cache options on the HTTP response.
+    /// </summary>
     public interface IResponseCacheFeature
     {
+        /// <summary>
+        /// Gets or sets the query keys used by the response cache middleware for calculating secondary vary keys.
+        /// </summary>
         StringValues VaryByQueryKeys { get; set; }
     }
 }
